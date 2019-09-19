39c4625108e57941f9764b0adb1721cce9702b5d
Craig Erskine <craig@craigerskine.com>
2019-09-18 19:21:29 -0500

    Update autostart.sh

--- configs/all/autostart.sh
+++ configs/all/autostart.sh
@@ -1,5 +1,5 @@
 # NAS
-#sudo mount -t cifs -o username=xxxxxx,password=xxxxxx,vers=1.0 //qmedia/games/games /home/pi/RetroPie/roms
+#sudo mount -t cifs -o username=xxxxxx,password=xxxxxx,vers=1.0 //qmedia/games/games/home/pi/RetroPie/roms
 # USB
 sudo mount --bind /media/usb0/System/home/pi/RetroPie/roms
 emulationstation #auto
